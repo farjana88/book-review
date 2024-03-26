432da3160b220056bd013b9a2a79973e20509fda
farjana88 <programmerfarjana1988@gmail.com>
2024-03-26 07:49:58 +0600

    added book section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 
 
 const Banner = () => {
@@ -8,13 +10,18 @@ const Banner = () => {
     <img src="../assets/pngwing 1.jpg" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">Books to freshen up <br /> your bookshelf</h1>
-    
+      <Link
+    to='/listed books'>
+     
       <button className="btn bg-[#23BE0A] text-white mt-10">View The List</button>
+      </Link>
+      
     </div>
+
   </div>
 </div>
 
-  <div className="text-3xl font-bold flex max-w-full justify-center items-center">Books</div>
+  <div className="text-3xl font-bold flex max-w-full justify-center items-center my-20">Books</div>
         </div>
         
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "../components/Banner";
+import Books from "../components/Books";
+
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Books></Books>
         </div>
     );
 };
